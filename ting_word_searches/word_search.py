18ff1e1f79f43f652f26319ff793fc3595614c16
Mandytrajano90 <mandytrajano90@gmail.com>
2024-04-12 18:25:01 -0300

    Seventh and Eighth Reqs - feat word_search

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,36 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    results = list()
+    for item in instance.queue:
+        path_name = item["nome_do_arquivo"]
+        word_exists = list()
+        with open(path_name, "r") as file:
+            for line_number, line in enumerate(file, start=1):
+                if word.lower() in line.lower():
+                    word_exists.append({"linha": line_number})
+        if word_exists:
+            results.append({
+                "palavra": word,
+                "arquivo": path_name,
+                "ocorrencias": word_exists
+            })
+    return results
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    results = list()
+    for item in instance.queue:
+        path_name = item["nome_do_arquivo"]
+        word_exists = list()
+        with open(path_name, "r") as file:
+            for line_number, line in enumerate(file, start=1):
+                if word.lower() in line.lower():
+                    word_exists.append(
+                        {"linha": line_number, "conteudo": line.strip()}
+                    )
+        if word_exists:
+            results.append({
+                "palavra": word,
+                "arquivo": path_name,
+                "ocorrencias": word_exists
+            })
+    return results
