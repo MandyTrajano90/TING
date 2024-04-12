bd19bca2de85818e2f997d69b4a8352aefdd159e
Mandytrajano90 <mandytrajano90@gmail.com>
2024-04-12 17:31:14 -0300

    Third Req - feat implementing process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,20 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    txt_importer(path_file)
+
+    if any(file_name == path_file for file_name in instance.queue):
+        return
+
+    instance.enqueue(path_file)
+    dict_data = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(txt_importer(path_file)),
+        "linhas_do_arquivo": txt_importer(path_file),
+    }
+    sys.stdout.write(f"{dict_data}\n")
 
 
 def remove(instance):
